ab9dba7d76d168bbd763443ef43f89485d2c8960
denis springinsfeld <denis.springinsfeld@unilim.fr>
2025-02-11 17:49:47 +0100

    Tw

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://denis-springinsfeld.github.io",
   integrations: [mdx(), sitemap()],
 });
